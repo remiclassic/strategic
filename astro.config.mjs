0d8eb0ebac03e4186277c81280b666b549ff09ab
Remi <rcout@example.com>
2025-04-06 20:02:32 -0400

    Update Astro and Vercel configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    analytics: false,
-    imageService: true,
-    devImageService: 'sharp'
-  }),
-  integrations: [tailwind()]
+  adapter: {
+    name: '@astrojs/vercel',
+    config: {
+      analytics: true,
+      imageService: true,
+      devImageService: 'sharp',
+      maxDuration: 10
+    }
+  },
+  integrations: [tailwind()],
+  build: {
+    assets: '_assets',
+    format: 'directory'
+  }
 });

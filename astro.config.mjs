836c06328a381870d4d8fdd5f671699c08d34ea9
Remi <rcout@example.com>
2025-04-06 20:45:18 -0400

    Update configuration for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://remiclassic.github.io',
-  base: '/strategic',
   output: 'static',
+  adapter: vercel({
+    analytics: false,
+    imageService: true,
+    devImageService: 'sharp'
+  }),
   integrations: [tailwind()],
   build: {
     assets: '_assets',

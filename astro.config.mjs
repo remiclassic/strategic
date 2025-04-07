4cb92f1d4f3babc6fe48137b158589b4692b538c
Remi <rcout@example.com>
2025-04-06 20:50:50 -0400

    Update Astro config for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    analytics: false,
-    imageService: true,
-    devImageService: 'sharp'
-  }),
+  base: '/strategic/',
   integrations: [tailwind()],
   build: {
     assets: '_assets',
